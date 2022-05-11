474fb60ee20d578a54e5693a1b7ca0ddc43a507e
EtafQudsi <etafkodsi@gmail.com>
2022-05-11 22:46:38 +0200

    last

--- Sheet 7/src/Query06.java
+++ Sheet 7/src/Query06.java
@@ -1,17 +1,12 @@
-import Tables.Employee;
+
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
-public class main {
+public class Query06 {
 
     public static void main(String[] args) {
         EntityManager em=Persistence.createEntityManagerFactory("Sheet_7PU").createEntityManager();
-        List<Employee> emp=em.createNamedQuery("Employee.findAll").getResultList();
-        for(Employee e:emp)
-        {
-            System.out.println(e.getFname()+" "+e.getSalary());
-        }
     }
     
 }
---
+++ Sheet 7/src/Query09.java
@@ -0,0 +1,12 @@
+
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+
+public class Query09 {
+
+    public static void main(String[] args) {
+        EntityManager em=Persistence.createEntityManagerFactory("Sheet_7PU").createEntityManager();
+    }
+    
+}
