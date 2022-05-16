0de19aca2fcd6a91f4a6567bc24c90d7a33d4543
Nesreen Ahmed El Tohamy <83316246+Nesreen-ahmed@users.noreply.github.com>
2022-05-16 17:16:57 +0200

    update

--- Sheet 7/src/Query09.java
+++ Sheet 7/src/Query09.java
@@ -1,29 +1,54 @@
 
-import Tables.Dependent;
-import Tables.Employee;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import Tables.Dependent;
+import Tables.Employee;
+import java.util.Collection;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Tuhamy
+ */
 public class Query09 {
 
     public static void main(String[] args) {
-        EntityManager em=Persistence.createEntityManagerFactory("Sheet_7PU").createEntityManager();
+        // TODO code application logic here
+        EntityManager em = Persistence.createEntityManagerFactory("TryProject").createEntityManager();
         em.getTransaction().begin();
-        
+
         List<Employee> Emps = em.createNamedQuery("Employee.findAll").getResultList();
         List<Dependent> Dep = em.createNamedQuery("Dependent.findAll").getResultList();
-        System.out.println("\n9.List the name of each employee and his/her spouse.\n\n\t--------------------\t----------------------");
-        System.out.println("\t  Employee Name \t\tSpouse Name");
-        
-        for(Dependent d: Dep)
+
+        System.out.println("\n9.List the name of each employee and his/her spouse.\n");
+        System.out.format("\t%5s ,\t%5s\n","------------------------","----------------------");
+        System.out.format("\t%18s,\t%25s\n","Employee Name", "Spouse Name");
+
+        for (Employee e : Emps) 
         {
-            if(d.getRelationship().equalsIgnoreCase("Spouse"))
+            System.out.format("\t%5s,\t%5s\n","------------------------","----------------------");
+            System.out.format("\t%5s",e.getFname() + "." + e.getMinit() + "." + e.getLname());
+
+            Collection<Dependent> depen = e.getDependentCollection();
+            if (depen.size() == 0) {
+                System.out.format("%30s\n","No spouse");
+            } 
+            else 
             {
-                System.out.println("\t--------------------\t----------------------");
-                System.out.println("\t  "+ d.getEmployee().getFname() +"\t\t\t"+d.getDependentPK().getDependentname());
+                for (Dependent dp : depen) 
+                {
+                    if (dp.getRelationship().equalsIgnoreCase("spouse")) 
+                    {
+                        System.out.format("%30s\n",dp.getDependentPK().getDependentname());
+                    }
+                }
             }
         }
+        System.out.format("\t%5s,\t%5s\n","------------------------","----------------------\n");
     }
-    
 }
